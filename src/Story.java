76201767265bfe06bceaafa0745adff60c131c55
E_DIDDY <E_DIDDY@10.0.0.68>
2018-08-02 10:54:52 -0400

    Updated with "David"

--- src/Story.java
+++ src/Story.java
@@ -5,10 +5,11 @@ public class Story {
 		// TODO Auto-generated method stub
 
 		System.out.println(
-				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of students.");
-		
-		System.out.println("Kai-Yun and his students were on a very dangerous mission. They had to be stealthy. They had to be quick.");
-		
+				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named David and his trusty band of students.");
+
+		System.out.println(
+				"David and his students were on a very dangerous mission. They had to be stealthy. They had to be quick.");
+
 		System.out.println("This rescue mission would be dangerous, but the monkeys had been preparing.");
 	}
 }
