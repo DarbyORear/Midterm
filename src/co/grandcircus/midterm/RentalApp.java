c39567d0226877487704b572f43887b527164352
DarbyORear <darbyorear@gmail.com>
2018-08-02 15:02:25 -0400

    ?

--- src/co/grandcircus/midterm/RentalApp.java
+++ src/co/grandcircus/midterm/RentalApp.java
@@ -1,74 +1,116 @@
 package co.grandcircus.midterm;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class RentalApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String cont;
+		boolean resume;
+		
 		Scanner scnr = new Scanner(System.in);
-
-		System.out.println("Welcome to Away for the Day!\n");
-
-		System.out.println(
-				"We are a company based in Detroit that operates an online marketplace\nand hospitality service for people to rent short-term lodging");
-
-		String humanName = Validator.getString(scnr,
-				"I'd love to know who I'm working with. Please provide me with your full name (first and last) so I know with whom I'm working with:\n");
-
-		System.out.println("\nThanks, " + humanName
-				+ "!\nWe can now start the process of finding you a lovely place to rest your head for a few nights.\n");
-
-		System.out.println("Go ahead and check out our lovely list of properies below\n");
-
-		System.out.println("Here are the currently Countries:\n");
-		// printList();
-
-		System.out.println("Hello! Welcome to Away for the Day. How can we help you escape today?: ");
+		do {
+		System.out.println("Hello! Welcome to Away for the Day.");
 		System.out.println("1. View all properties\n");
 		System.out.println("2. Search properties by location\n");
 		System.out.println("3. Search properties by availability\n");
-		System.out.println("4. Search properties by rental type\n\n");
-		int userChoice = scnr.nextInt();
-
-		if (userChoice == 1) {
-			// need to create method for displaying properties
+		System.out.println("4. Search properties by rental type\n");
+		System.out.println("5. Exit the program\n");
+		int userChoice = Validator.getInt(scnr,"How can we help you escape today?: ", 1, 5);
+		
+		if(userChoice == 1) {
+			//need to create method for displaying properties
 			viewProperties();
-		} else if (userChoice == 2) {
-			// need to create method for searching by properties
+//			System.out.println("Would you like to choose another option");
+		} else if(userChoice == 2) {
+			//need to create method for searching by properties
 			searchByLocation();
-		} else if (userChoice == 3) {
-			// need to create method for searching by properties
+		} else if(userChoice == 3) {
+			//need to create method for searching by properties
 			searchByAvailability();
 		} else {
 			searchByPropertyType();
 		}
-
+		cont = Validator.getStringMatchingRegex(scnr, "Would you like to choose another option? (y/n): ", "Y|yes|N|no");
+			if(cont.toLowerCase().startsWith("y")) {
+				resume = true;
+			} else {
+				resume = false;
+				break;
+			}
+	}while(resume);
 	}
 
-	// reservation confirmation method:
-	public void ReservationConfirmation(/* will it take an argument? */) {
-		String renterName = "";
-		String renterFirstName = "";
-		String renterLastName = "";
-		int rentalPrice = 0;
-		String propertyName = "";
-		String propertyLocation = "";
-		int lengthOfStay = 0;
-		LocalDate checkInDate;
-		LocalDate checkOutDate;
 
-		// Thank user for booking with us:
-		System.out.println("Thanks for booking with us, " + renterName + "!\n");
-		System.out.println("Here are the details of your researvation:\n");
 
-		System.out.println("Name: " + renterFirstName + renterLastName + "\n");
-		System.out.println("Price: " + "$" + rentalPrice + "\n");
-		System.out.println("Property: " + propertyName + "\n");
-		System.out.println("Location: " + propertyLocation + "\n");
-		System.out.println("Duration of rental: " + lengthOfStay + "days.\n");
-		System.out.println("Rental dates: " + checkInDate + checkOutDate + "\n");
 
+
+
+public static void searchByPropertyType() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+private static void searchByAvailability() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+public static void searchByLocation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+public static void viewProperties() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+
+
+//reservation confirmation method:
+public void ReservationConfirmation(/*will it take an argument?*/) {
+	String renterName="";
+	String renterFirstName="";
+	String renterLastName="";
+	int rentalPrice=0;
+	String propertyName= "";
+	String propertyLocation = "";
+	int lengthOfStay = 0;
+	LocalDate checkInDate;
+	LocalDate checkOutDate;
+	
+	//Thank user for booking with us:
+	System.out.println("Thanks for booking with us, " + renterName + "!\n");
+	System.out.println("Here are the details of your researvation:\n");
+	
+	System.out.println("Name: " + renterFirstName + renterLastName + "\n");
+	System.out.println("Price: " + "$" + rentalPrice + "\n");
+	System.out.println("Property: " + propertyName + "\n");
+	System.out.println("Location: " + propertyLocation + "\n");
+	System.out.println("Duration of rental: " + lengthOfStay + "days.\n");
+	System.out.println("Rental dates: " + checkInDate + checkOutDate + "\n");
+
+	
 	}
 }
