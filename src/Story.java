4f23ab248d8e40bf9c655272beb1d8c600ae8c4a
Nightowl93 <shontini@umich.edu>
2018-08-02 10:53:12 -0400

    Conflict

--- src/Story.java
+++ src/Story.java
@@ -5,9 +5,9 @@ public class Story {
 		// TODO Auto-generated method stub
 
 		System.out.println(
-				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+				"Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of students.");
 		
-		System.out.println("Kai-Yun and his ninja monkeys were on a very dangerous mission. They had to be stealthy. They had to be quick.");
+		System.out.println("Kai-Yun and his students were on a very dangerous mission. They had to be stealthy. They had to be quick.");
 		
 		System.out.println("This rescue mission would be dangerous, but the monkeys had been preparing.");
 	}
