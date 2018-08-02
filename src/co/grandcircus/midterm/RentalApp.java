be09278d870ec072e46024110355578ca334e0db
DarbyORear <darbyorear@gmail.com>
2018-08-02 13:44:50 -0400

    created method for reservation confirmation

--- src/co/grandcircus/midterm/RentalApp.java
+++ src/co/grandcircus/midterm/RentalApp.java
@@ -1,5 +1,8 @@
 package co.grandcircus.midterm;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class RentalApp {
 
 	public static void main(String[] args) {
@@ -7,4 +10,34 @@ public class RentalApp {
 
 	}
 
+
+
+
+
+
+//reservation confirmation method:
+public void ReservationConfirmation(/*will it take an argument?*/) {
+	String renterName="";
+	String renterFirstName="";
+	String renterLastName="";
+	int rentalPrice=0;
+	String propertyName= "";
+	String propertyLocation = "";
+	int lengthOfStay = 0;
+	LocalDate checkInDate;
+	LocalDate checkOutDate;
+	
+	//Thank user for booking with us:
+	System.out.println("Thanks for booking with us, " + renterName + "!\n");
+	System.out.println("Here are the details of your researvation:\n");
+	
+	System.out.println("Name: " + renterFirstName + renterLastName + "\n");
+	System.out.println("Price: " + "$" + rentalPrice + "\n");
+	System.out.println("Property: " + propertyName + "\n");
+	System.out.println("Location: " + propertyLocation + "\n");
+	System.out.println("Duration of rental: " + lengthOfStay + "days.\n");
+	System.out.println("Rental dates: " + checkInDate + checkOutDate + "\n");
+
+	
+	}
 }
